de64d048e595a5eded5869ded400b1bea2403c14
Maliha Mim <malihamim.info@gmail.com>
2023-12-24 15:53:01 +0600

    [UPDATE] banner

--- bdtask_custom_title_and_favicon/__manifest__.py
+++ bdtask_custom_title_and_favicon/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Custom Title and Favicon',
-    'version': '16.0.8',
+    'version': '16.0.9',
     'sequence': 1,
     'summary': """
         Custom Title and Favicon
@@ -16,7 +16,7 @@
     'website': 'https://www.bdtask.com/',
     'license': 'LGPL-3',
     'images': [
-        'static/description/banner_icon.png'
+        'static/description/banner03.png'
     ],
     'depends': [
         'web'
